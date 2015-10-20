cb650f03007b364b9b6dced385d39416940df4d9
Zaargh <sailor88@gmail.com>
2015-10-20 19:42:49 +0000

    Error handling implemantation: no of args, wrong dirs.

--- sedcp.sh
+++ sedcp.sh
@@ -14,5 +14,41 @@
 #   * sygnalizować użycie niepoprawnego argumentu (np. nie istniejący katalog),
 #   * sygnalizować brak odpowiednich praw dostępu.
 
-find $2 -name \*$1 -print0 | xargs -0 -I {} cp {} $3
+FROM="$2"
+TO="$4"
+EXTFROM="$1"
+EXTTO="$3"
+
+if [ $# -ne 4 ]; then
+    echo "Error: Invalid number of parameters. Correct call: $0 .ext1 source_path .ext2 destination_path"
+    exit 1
+fi
+
+if [ ! -d "$FROM" ]; then
+    echo "Error: $FROM : Not a directory."
+    exit 1
+fi
+if [ ! -d "$TO" ]; then
+    echo "Error: $TO : Not a directory."
+    exit 1
+fi
+
+if [ ! -r "$FROM" ]; then
+    echo "Error: $FROM : Cannot read from directory."
+    exit 1
+fi
+if [ ! -w "$TO" ]; then
+    echo "Error: $TO : Cannot write to directory."
+    exit 1
+fi
+
+
+
+#TODO Lack of dot error
+
+
+FILELIST=`find $FROM -name \*$EXTFROM`
+
+
+echo "${FILELIST}" | xargs -I {} cp {} $3
 #| sed 's/'$1'/'$3'/'  
